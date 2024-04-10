7a6a3c6647906b497065e73ce62b77b801a03bab
pipef104 <pipef104@gmail.com>
2024-04-10 20:57:00 +0000

    First commit on desarrollo

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("devfelipe.invoices.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
